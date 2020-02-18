4ac9872f9545ef32756108c4774905f9c1aed66e
ritter <you@example.com>
2020-02-18 11:47:37 -0600

    toolbar and about module added

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,6 +7,7 @@ import { MatToolbarModule }  from '@angular/material/toolbar';
 import { MatIconModule }  from '@angular/material/icon';
 import { MatSidenavModule }  from '@angular/material/sidenav';
 import { MatListModule }  from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 const modules = [
   MatCardModule, 
@@ -15,7 +16,8 @@ const modules = [
   MatToolbarModule, 
   MatIconModule, 
   MatSidenavModule, 
-  MatListModule
+  MatListModule,
+  MatGridListModule
 ];
 
 @NgModule({
